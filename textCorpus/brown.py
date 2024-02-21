b7eaece178f094e82c08781844df7f262fd64514
Naman <namanmalpani2399@gmail.com>
2024-02-21 21:28:10 +0530

    Added Reverse Mapping in textCorpus

--- textCorpus/brown.py
+++ textCorpus/brown.py
@@ -3,20 +3,28 @@ from nltk.corpus import brown
 from nltk.corpus import stopwords
 
 
-# Output : Nested list token words and mapping for tokens 
+'''
+Output : Nested list token words
+         mapping : Key : Word  Value : Index
+         reverse_mapping : Key : Index , Value : Word
+'''
+
+           
 def dataset() :
-    # print("Importing Brown dataset")
+    print("Importing Brown dataset")
     # print("Total length of words : ", len(brown.words()))
     # print("50 sample words from dataset : " , brown.words())
+    nltk.download('brown')
     sentences = brown.sents() 
     # print("Total sentences : ", len(sentences))
     dataset = data_preprocessing(dataset= sentences)
     # print("Sample dataset sentence :  " , dataset[100])
-    mapping, words  = get_vocab(dataset= dataset)
+    mapping , words  = get_vocab(dataset= dataset)
     dataset_tokens = convert_word_to_index(dataset= dataset, mapping= mapping)
+    reverse_mapping = reverse_dict(mapping= mapping)
     # print("Sample dataset sentence token :  " , dataset_tokens[100])
 
-    return dataset_tokens, mapping
+    return dataset_tokens, mapping, reverse_mapping
     
 
 # Input : Nested of list of sentences
@@ -66,18 +74,28 @@ def convert_word_to_index(dataset : list, mapping : dict) :
     
     return dataset 
 
+
+# Input : Key : Word , Value : Index 
+# Output : Key : Index, value : word
+def reverse_dict(mapping : dict) : 
+    reverse_map = {} 
+    key_ls = list(mapping.keys()) 
+    for i in range(len(key_ls)) : 
+        word = key_ls[i] 
+        idx = mapping[word]
+        reverse_map[idx] = word
+
+    return reverse_map
  
 '''
 Driver code logic 
 Just call dataset function to get all the sentence in |V| length where each word is replace by corresponding index 
-'''
-dataset_token, mapping = dataset()
+
+dataset_token, mapping, reverse_mapping = dataset()
+print(dataset_token[100])
+idx = 812
+print("Word at index : ", idx , " - ", reverse_mapping[idx])
+print("Index for word : ", reverse_mapping[idx],  " - " , idx)
 
 
 '''
-One time run python file 
-nltk.download('brown')
-nltk.download('stopwords')
-
-'''
-
