e0b4e1b4cbd7afc5d18922b439fc7c5f89ce07e1
Naman <namanmalpani2399@gmail.com>
2024-03-07 22:17:23 +0530

    Updated the RNN Pytorch Model

--- models/rnn_pytorch.py
+++ models/rnn_pytorch.py
@@ -1,12 +1,11 @@
 # Import  libraries
 import sys
-sys.path.insert(1,'../')
 import torch
 from torch import nn
-import torch.nn.functional as torchFunctional
 import textCorpus.brown as brown
 from torchsummary import  summary
-from sklearn.model_selection import train_test_split
+import utilities as utilities
+sys.path.insert(1,'../')
 
 
 '''
@@ -16,22 +15,6 @@ Input  : Sentence (List of tokens for a sentence)
 
 Output : Return the nested list of token in one hot vector form 
 '''
-
-
-
-def convert_dataset_to_tensor(sentence: list, mapping: dict, reverse_mapping: dict):
-
-    num_classes = len(list(mapping.keys())) + 1
-    # print("Vocabulary Length : ", num_classes)
-    # print("Input Sentence : " , sentence)
-    sentence_tensor = torch.tensor(sentence)
-    sentence_token_one_hot = torchFunctional.one_hot(sentence_tensor, num_classes=num_classes)
-    # print("Input One Hot : ", sentence_token_one_hot)
-    # print("Input One Hot Shape : ", sentence_token_one_hot.shape)
-    return sentence_token_one_hot
-
-
-
 
 class RNN(nn.Module) :
     def __init__(self, input_size , embedding_size, hidden_size, output_size):
@@ -55,98 +38,75 @@ class RNN(nn.Module) :
 
     def forward(self, input, hidden_state):
         e = self.embedding(input)
-        print(e.shape)
         h1 = self.weight(e)
-        print(h1.shape)
         h2 = self.u(hidden_state)
-        print(h2.shape)
         h = torch.add(h1, h2)
-        print(h.shape)
         out = self.v(h)
-        print(out.shape)
         out = torch.softmax(out, dim = 1 )
-        print(out.shape)
         out.reshape(-1)
-        print(out.shape)
         return out, h
 
     def init_hidden(self):
         return nn.init.kaiming_uniform_(torch.empty(1, self.hidden_size))
 
 
-dataset_token, mapping, reverse_mapping = brown.dataset()
-sentence_token_one_hot = convert_dataset_to_tensor(sentence= dataset_token[0], mapping= mapping, reverse_mapping= reverse_mapping)
-print("Starting One hot conversion")
-dataset_one_hot = []
-# length = len(dataset_token)
-length = 100
-for i in range(length) :
-    dataset_one_hot.append(convert_dataset_to_tensor(sentence = dataset_token[i], mapping= mapping, reverse_mapping= reverse_mapping))
-print("Completed One hot conversion")
-print("Dataset : ", len(dataset_one_hot))
-print("dataset ------------> ", dataset_one_hot[0].shape)
 
+
+dataset, mapping, reverse_mapping = brown.dataset()
+train_dataset, test_dataset = brown.train_test_slit(dataset)
 print("Starting Train Test Split")
 # Splitting dataset into train, test, validation
-train_idx, test_idx = train_test_split(
-    range(len(dataset_one_hot)),
-    test_size=0.1,
-    shuffle=True,
-)
 
-train_dataset = [
-    (dataset_one_hot[i])
-    for i in train_idx
-]
-
-test_dataset = [
-    (dataset_one_hot[i])
-    for i in test_idx
-]
-
-print("Train Split : ", len(train_dataset))
-print("dataset ------------> ", train_dataset[0].shape)
-print("Test Split : ", len(test_dataset))
-print("dataset ------------> ", test_dataset[0].shape)
-
-
-input_size = sentence_token_one_hot.shape[1]
+input_size = len(mapping)
 embedding_size = 300
 hidden_size = 256
-output_size = sentence_token_one_hot.shape[1]
-learning_rate = 0.001
+output_size = input_size
+learning_rate = 0.01
 epochs = 11
+mini_batch_size = 1024
 
 model = RNN(input_size = input_size, embedding_size= embedding_size, hidden_size= hidden_size, output_size= output_size)
 criterion = nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(model.parameters(), lr = learning_rate)
+optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
 print("Model Summary")
 print(summary(model, input_size = [(1, input_size), (1, hidden_size)] , batch_size= -1))
 
 for epoch in range(epochs) :
-    for i in range(2) : # len(train_dataset)
+    model.train()
+    mini_batches = len(train_dataset) // mini_batch_size
+    idx = 0
+    for mini_batch in range(mini_batches) :
+        mini_batch_loss = 0
         hidden_state = model.init_hidden()
-        print("hedskfjsdf", hidden_state.shape)
-        sentence = train_dataset[i]
-        print(sentence)
-        print(type(sentence))
-        print(sentence.shape)
-        for i in range(sentence.shape[0]-1) :
-            input_vector = sentence[i].float().reshape(1, -1)
-            output_vector = sentence[i+1].float().reshape(1, -1)
-            print("Input Shape : ", input_vector.shape)
-            print("Output Shpae : ", output_vector.shape)
-            output_hat, hidden_state = model(input_vector, hidden_state)
-            curr_loss = criterion(output_hat, output_vector)
-            total_loss = curr_loss
-
+        total_dividend = 0
+        for _ in range(mini_batch_size) :
+            sentence = train_dataset[idx]
+            idx += 1
+            idx = idx % len(train_dataset)
+            # Converting two one-hot vectors
+            sentence = brown.convert_sentence_to_one_hot_tensor(sentence= sentence, mapping= mapping)
+            sentence_loss = 0
+            total_dividend += len(sentence) - 1
+            for sentence_idx in range(len(sentence) - 1) :
+                input_vector = sentence[sentence_idx].float().reshape(1, -1)
+                output_vector = sentence[sentence_idx + 1].float().reshape(1, -1)
+                output_hat, hidden_state = model(input_vector, hidden_state)
+                loss = criterion(output_vector, output_hat)
+                if sentence_loss == 0 :
+                    sentence_loss = loss
+                else :
+                    sentence_loss += loss
+            if mini_batch_loss == 0 :
+                mini_batch_loss = sentence_loss
+            else :
+                mini_batch_loss += sentence_loss
         optimizer.zero_grad()
-        total_loss.backward()
+        mini_batch_loss /= total_dividend
+        mini_batch_loss += utilities.l2_loss(model, lambda_l2= 0.01)
+        mini_batch_loss.backward()
         optimizer.step()
+        print(f"Epoch : {epoch + 1}, Min-batch : {mini_batch + 1}, training-loss : {mini_batch_loss}")
 
-    print(epoch)
-
-print("Kuch hua hai")
 
 
 
--- textCorpus/brown.py
+++ textCorpus/brown.py
@@ -1,6 +1,8 @@
 import nltk
 from nltk.corpus import brown
-from nltk.corpus import stopwords
+from sklearn.model_selection import train_test_split
+import torch
+import torch.nn.functional as torchFunctional
 
 
 '''
@@ -12,23 +14,32 @@ Output : Nested list token words
            
 def dataset() :
     print("Importing Brown dataset")
-    # print("Total length of words : ", len(brown.words()))
-    # print("50 sample words from dataset : " , brown.words())
     nltk.download('brown')
     sentences = brown.sents() 
-    # print("Total sentences : ", len(sentences))
     dataset = data_preprocessing(dataset= sentences)
-    # print("Sample dataset sentence :  " , dataset[100])
+    dataset = converting_dataset_fixed_sequence_length(dataset= dataset)
     mapping , words  = get_vocab(dataset= dataset)
     dataset_tokens = convert_word_to_index(dataset= dataset, mapping= mapping)
     reverse_mapping = reverse_dict(mapping= mapping)
-    # print("Sample dataset sentence token :  " , dataset_tokens[100])
 
     return dataset_tokens, mapping, reverse_mapping
     
+'''
+Input : Nested of list of sentences
+Output : Nested of list of sentences
+'''
 
-# Input : Nested of list of sentences
-# Output : Nested of list of sentences
+def converting_dataset_fixed_sequence_length(dataset, sequence_length = 5) :
+    converted_dataset = []
+    for i in range(len(dataset)) :
+        if len(dataset) <= 5 :
+            converted_dataset.append(dataset[i])
+        else :
+            for j in range(len(dataset[i]) - sequence_length) :
+                converted_dataset.append(dataset[i][j:j+sequence_length])
+                j += 2
+    return converted_dataset
+
 
 def data_preprocessing(dataset : list) :
     # Lower case conversion
@@ -43,8 +54,11 @@ def data_preprocessing(dataset : list) :
 
     return output
 
-# Input : Nested of list of sentences
-# Output : Dict of words where key : word and value : word index and vocbulary list 
+'''
+Input : Nested of list of sentences
+Output : Dict of words where key : word and value : word index and vocbulary list 
+'''
+
 def get_vocab(dataset : list) : 
     
     mapping = {} # Key : Word maps to value : frequency 
@@ -58,7 +72,7 @@ def get_vocab(dataset : list) :
     words = list(mapping.keys()) 
     mapping = {} # Key : Word and value : Index 
     for i in range(len(words)) : 
-        mapping[words[i]] = i+1 
+        mapping[words[i]] = i
 
     return mapping , words 
 
@@ -77,16 +91,46 @@ def convert_word_to_index(dataset : list, mapping : dict) :
 
 # Input : Key : Word , Value : Index 
 # Output : Key : Index, value : word
-def reverse_dict(mapping : dict) : 
+def reverse_dict(mapping : dict) :
+
     reverse_map = {} 
     key_ls = list(mapping.keys()) 
     for i in range(len(key_ls)) : 
-        word = key_ls[i] 
+        word = key_ls[i]
         idx = mapping[word]
         reverse_map[idx] = word
 
     return reverse_map
- 
+
+
+def train_test_slit(dataset, test_size = 0.1) :
+     train_idx, test_idx = train_test_split(
+         range(len(dataset)),
+         test_size=0.1,
+         shuffle=True,
+     )
+
+     train_dataset = [
+         (dataset[i])
+         for i in train_idx
+     ]
+
+     test_dataset = [
+         (dataset[i])
+         for i in test_idx
+     ]
+
+     print("Train Split : ", len(train_dataset))
+     print("Test Split : ", len(test_dataset))
+
+def convert_sentence_to_one_hot_tensor(sentence: list, mapping: dict):
+
+    num_classes = len(list(mapping.keys()))
+    sentence_tensor = torch.tensor(sentence)
+    sentence_token_one_hot = torchFunctional.one_hot(sentence_tensor, num_classes=num_classes)
+    sentence_token_one_hot = sentence_token_one_hot.float()
+    return sentence_token_one_hot
+
 '''
 Driver code logic 
 Just call dataset function to get all the sentence in |V| length where each word is replace by corresponding index 
